f48063e2293bc985b3d04c8b1e576a34e4bf99f2
Marshall Ku <taengoo890309@naver.com>
2020-01-03 15:44:50 +0900

    Create titletyper.js

---
+++ titletyper.js
@@ -0,0 +1,53 @@
+function typeTitle(title, animation, speed) {
+  const isString = typeof(title) === "string";
+  let i = 0, j = 0;
+  speed === undefined && (
+    speed = 500
+  ),
+
+  function txtnum() {
+    j == textArray.length - 1
+    ? j = 0
+    : j++
+  }
+
+  function type() {
+    const max = isString ? title.length : title[j].length;
+    i <= max
+    ? (
+      document.title = `\u200E${(isString ? title : title[j]).slice(0, i)}`,
+      i++,
+      setTimeout(type, speed)
+    )
+    : (
+      animation.indexOf("type-") === -1
+      ? remove()
+      : (
+        animation === "type-once" || (
+          setTimeout(() => {
+            document.title = "\u200E",
+            i = 0,
+            type()
+          }, +animation.replace("type-", ""))
+        )
+      )
+    )
+  }
+
+  function remove() {
+    0 < i
+    ? (
+      document.title = document.title.slice(0, i),
+      i--,
+      0 == i && (document.title = "\u200E"),
+      setTimeout(remove, speed)
+    )
+    : (
+      isString || txtnum(),
+      type()
+    )
+  }
+
+  document.title = "\u200E",
+  type();
+}
